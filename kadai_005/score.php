5bee2de1e6cfca96df344e96c2aef670ada6d497
anpan-jampan <91524825+anpan-jampan@users.noreply.github.com>
2024-08-04 08:10:53 +0900

    Add files via upload
    005

---
+++ kadai_005/score.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>kadai_005</title>
+    <meta charset="UTF-8">
+    </head>
+
+    <body>
+        <?php
+        $score1 = 80;
+        $score2 = 60;
+        $score3 = 55;
+        $score4 = 40;
+        $score5 = 100;
+        $score6 = 25;
+        $score7 = 80;
+        $score8 = 95;
+        $score9 = 30;
+        $score10 = 60;
+
+        $avr = $score1+$score2+$score3+$score4+$score5+$score6+$score7+$score8+$score9+$score10;
+        echo $avr/10;
+
+        ?>
+    </body>
+</html>
