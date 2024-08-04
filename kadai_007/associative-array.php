6959d420d0d6751f7711021dab43061268764fdd
anpan-jampan <91524825+anpan-jampan@users.noreply.github.com>
2024-08-04 11:41:55 +0900

    Add files via upload
    007

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    </head>
+
+    <body>
+        <?php 
+        $vegitable = ["name" => "onion", "price" => 200, "weight" => 160];
+        print_r($vegitable) ;
+         ?>
+    </body>
+
+</html>
